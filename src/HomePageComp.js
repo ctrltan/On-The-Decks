eb2c1c6ecf8be5efd2dbee4df36d972560e61a64
Stephanie Lawal <stephanieelawal@gmail.com>
2024-12-08 21:52:12 +0000

    add first component

---
+++ src/HomePageComp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomeComponent = () => {
+    return (
+        <div>
+            <h1>This is On The Decks. A comprehensive breakdown of your music this year!</h1>
+        </div>
+    );
+}
+export default HomeComponent;
