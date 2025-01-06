09a427767bfd12b5f899d89356a491a3e537c38b
Stephanie Lawal <stephanieelawal@gmail.com>
2025-01-06 12:54:23 +0000

    organise project structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HomeComponent from './HomePageComp';
+import HomeComponent from './components/HomePageComp';
 
 function App() {
   return (
