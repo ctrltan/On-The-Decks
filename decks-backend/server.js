44f0d3de365ef5a2ee5dd0f8afd36e91598a490f
Stephanie Lawal <stephanieelawal@gmail.com>
2025-03-29 20:59:27 +0000

    Make api requests to spotify for user login and access token exchange

--- decks-backend/server.js
+++ decks-backend/server.js
@@ -1,11 +1,16 @@
-requiure('dotenv').config()
+require('dotenv').config()
 const crypto = require('crypto')
 const express = require('express')
 const axios = require('axios')
 const app = express()
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const { access } = require('fs')
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+}))
+app.use(cookieParser())
 
 const REDIRECT_URI = 'http://localhost:8080/callback'
 const PORT = 8080
@@ -19,10 +24,11 @@ app.get('/login', (req, res) => {
         const state = crypto.randomBytes(16).toString('hex');
         const scope = 'user-read-private playlist-read-private playlist-read-collaborative user-top-read user-library-read';
 
-        res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&scope=${scope}&redirect_uri=${REDIRECT_URI}&state=${state}`);
+        res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&state=${state}`);
     } catch(error) {
         console.error("Spotify Login Failed", error);
-        res.status(500).json({ success: false, message: 'Sorry! We could not log you in at the moment' });
+        const message = encodeURIComponent('Sorry! We could not log you in at this moment');
+        res.redirect(`http://localhost:3000?status=failure&message=${message}`);
     }
 });
 
@@ -31,13 +37,45 @@ app.get('/callback', async (req, res) => {
         const state = req.query.state || null
         const auth_code = req.query.code || null
 
-        if (state === null) {
+        if (state === null || auth_code === null) {
             throw 500;
-        } else {
-            
         }
+
+        const params = new URLSearchParams();
+        params.append('code', auth_code);
+        params.append('redirect_uri', REDIRECT_URI);
+        params.append('grant_type', 'authorization_code');
+
+        const response = await axios.post('https://accounts.spotify.com/api/token', params, {
+            headers: {
+                'Content_Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'),
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+
+        const { access_token, refresh_token } = response.data;
+
+        res.cookie('access_token', access_token, {
+            'httpOnly': true,
+            'secure': false,
+            'sameSite': 'strict',
+            'maxAge': 3600000,
+        });
+
+        res.cookie('refresh_token', refresh_token, {
+            'httpOnly': true,
+            'secure': false,
+            'sameSite': 'strict',
+            'maxAge': 2629476000,
+        });
+
+        const message = encodeURIComponent('It worked!');
+        res.redirect(`http://localhost:3000?status=success&message=${message}`);
     } catch(error) {
-        res.redirect();
+        console.error("Callback Failed", error);
+        const message = encodeURIComponent('Sorry! We could not log you in at this moment');
+        res.redirect(`http://localhost:3000?status=failure&message=${message}`);
     }
 });
 
